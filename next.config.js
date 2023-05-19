7c2dd66fcddf24ccd47c2ccf2131570e3e80e78d
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-05-20 00:56:05 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+    output: "standalone",
+    images: {
+        domains: ["flagcdn.com", "upload.wikimedia.org"],
+    }
 }
 
 module.exports = nextConfig
