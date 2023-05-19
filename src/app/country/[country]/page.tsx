c2300a7365d38eba3605ac067a3bb5d708d1f37f
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-05-20 00:59:09 +0100

    update page.tsx

--- src/app/country/[country]/page.tsx
+++ src/app/country/[country]/page.tsx
@@ -1,21 +1,150 @@
-import React from "react";
+import LinkButton from "@/components/Button/LinkButton";
+import SimpleGrid from "@/components/Grid/SimpleGrid";
+import Flex from "@/components/List/Flex";
+import { numberWithCommas } from "@/utils";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import LocalData from "../../../../data.json";
+import styles from "../../page.module.css";
 
 async function getCountry(name: string) {
-  const res = await fetch(`https://restcountries.com/v3.1/all?filedlsname=${name}`);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+  const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
 
-  // Recommendation: handle errors
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
   return res.json();
-};
+}
 
-const Page = ({ params }: { params: { country: string } }) => {
-  return <div>{params.country}</div>;
+const Page = async ({ params }: { params: { country: string } }) => {
+  const country: CountryProps[] = await getCountry(params.country).then(
+    (res) => {
+      return res;
+    }
+  );
+
+  const getCountryByCode = (code: string) => {
+    let res = "";
+    LocalData.filter((country) => {
+      if (country.alpha3Code === code) {
+        res = country.name;
+      }
+    });
+    return res;
+  };
+
+  const createLimitedList = (arr: string[], limit: number) => {
+    let newArr = [];
+    for (let i = 0; i < limit; i++) {
+      newArr.push(arr[i]);
+    }
+    if (arr.length > limit) {
+      return newArr;
+    } else {
+      return arr
+    }
+  };
+
+  function renderList(arr: string[]) {
+    return arr.map((a, i) => {
+      return (
+        <LinkButton
+          to={`/country/${getCountryByCode(a)}`}
+          key={i}
+          className={styles["link-btn"]}
+        >
+          {a}
+        </LinkButton>
+      );
+    });
+  }
+
+  const limitRender = (
+    arr: string[] | any[],
+    limit: number
+  ): any[] | string => {
+    const createdArr = createLimitedList(arr, limit);
+    return renderList(createdArr);
+  };
+
+  const getLocalData = (c: CountryProps, key: string) => {
+    let res = "";
+    let localCountry = LocalData;
+    localCountry.filter((country) => {
+      if (
+        key === "currency" &&
+        country.name === c.name.common &&
+        country.currencies !== undefined
+      ) {
+        res = country.currencies.map((c) => c.name).join(", ");
+      } else if (key === "languages") {
+        res = country.languages.map((c) => c.name).join(", ");
+      }
+    });
+    return res;
+  };
+
+  return (
+    <div className={styles.countries}>
+      <Flex>
+        <LinkButton to={"/"}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+          <span>Back</span>
+        </LinkButton>
+      </Flex>
+      <div className={styles["main-content"]}>
+        <div>
+          <div className={styles.flag}>
+            <Image
+              src={country[0].flags.svg}
+              alt="country_flag"
+              fill={true}
+              className={styles.cimg}
+            />
+          </div>
+          <div className={styles.description}>
+            <h1>{country[0].name.common}</h1>
+            <SimpleGrid row={5} column={2} columnGap={10} rowGap={5}>
+              <p>
+                Native Name: <span>{country[0].name.common}</span>
+              </p>
+              <p>
+                Population: <span>{numberWithCommas(country[0].population)}</span>
+              </p>
+              <p>
+                Region: <span>{country[0].region}</span>
+              </p>
+              <p>
+                Sub Region: <span>{country[0].subregion}</span>
+              </p>
+              <p>
+                Capital: <span>{country[0].capital}</span>
+              </p>
+              <p>
+                Top Level Domain: <span>{country[0].tld.join("")}</span>
+              </p>
+              <p>
+                Currencies: <span>{getLocalData(country[0], "currency")}</span>
+              </p>
+              <p>
+                Languages: <span>{getLocalData(country[0], "languages")}</span>
+              </p>
+            </SimpleGrid>
+            {country[0].borders !== undefined && (
+              <div className={styles.bc}>
+                <p>Border Countries:</p>
+                <div className={styles.mc}>
+                  {limitRender(country[0].borders, 3)}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
