4836664ff136733b8b1808080f51ee49631a9e89
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-05-20 00:58:40 +0100

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navigation/Navbar";
 import "./globals.css";
-import Script from "next/dist/client/script";
+import { ThemeProvider } from "@/context/ThemeContext";
+import Footer from "@/components/Navigation/Footer";
 
 export const metadata = {
   title: "Countries-Api",
@@ -14,10 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      {/**body tag is in the theme provider component */}
+      <ThemeProvider>
         <Navbar />
         {children}
-      </body>
+      </ThemeProvider>
     </html>
   );
 }
